8c487234cbddff3b8208dbd88c6700e8cf91b1ee
Kyle Lin <kylejlin.us@gmail.com>
2023-02-07 23:00:50 -0800

    Extract LoggableMenu

--- src/App.tsx
+++ src/App.tsx
@@ -6,56 +6,11 @@ function App({ stateProvider }: Props) {
   const stateHook = useState(
     stateProvider.getSavedState() ?? getDefaultState()
   );
-  const [state, setState] = stateHook;
+  const [state] = stateHook;
 
   useEffect(() => {
     stateProvider.saveState(state);
   });
-
-  const eventFilter = state.showRedactedEvents
-    ? (_event: Event) => true
-    : (event: Event) => !event.redacted;
-  const sortedEventsRecentFirst = state.events
-    .filter(eventFilter)
-    .sort((a, b) => b.time - a.time);
-
-  function redactEvent(eventTime: number) {
-    setState((state) => ({
-      ...state,
-      events: state.events.map((event) =>
-        event.time === eventTime ? { ...event, redacted: true } : event
-      ),
-    }));
-  }
-
-  function restoreEvent(eventTime: number) {
-    setState((state) => ({
-      ...state,
-      events: state.events.map((event) =>
-        event.time === eventTime ? { ...event, redacted: false } : event
-      ),
-    }));
-  }
-
-  function hideRedactedEvents() {
-    setState((state) => ({
-      ...state,
-      showRedactedEvents: false,
-    }));
-  }
-
-  function showRedactedEvents() {
-    setState((state) => ({
-      ...state,
-      showRedactedEvents: true,
-    }));
-  }
-
-  const toggleShowRedactedEventsButton = state.showRedactedEvents ? (
-    <button onClick={hideRedactedEvents}>Hide redacted</button>
-  ) : (
-    <button onClick={showRedactedEvents}>Show redacted</button>
-  );
 
   return (
     <div className="App">
@@ -63,25 +18,7 @@ function App({ stateProvider }: Props) {
 
       <LoggableMenu stateHook={stateHook} />
 
-      <h2>
-        Events ({sortedEventsRecentFirst.length}){" "}
-        {toggleShowRedactedEventsButton}
-      </h2>
-      <ul>
-        {sortedEventsRecentFirst.map((event) => (
-          <li
-            key={event.time}
-            className={"Event" + (event.redacted ? " Event--redacted" : "")}
-          >
-            {event.name} {<EventTime time={event.time} />}{" "}
-            {event.redacted ? (
-              <button onClick={() => restoreEvent(event.time)}>Restore</button>
-            ) : (
-              <button onClick={() => redactEvent(event.time)}>Redact</button>
-            )}
-          </li>
-        ))}
-      </ul>
+      <EventsMenu stateHook={stateHook} />
     </div>
   );
 }
@@ -194,6 +131,79 @@ function LoggableMenu({ stateHook }: StateHookProps) {
             <button onClick={addTentativeNewLoggable}>Add</button>
           </li>
         )}
+      </ul>
+    </>
+  );
+}
+
+function EventsMenu({ stateHook }: StateHookProps) {
+  const [state, setState] = stateHook;
+
+  const eventFilter = state.showRedactedEvents
+    ? (_event: Event) => true
+    : (event: Event) => !event.redacted;
+  const sortedEventsRecentFirst = state.events
+    .filter(eventFilter)
+    .sort((a, b) => b.time - a.time);
+
+  function redactEvent(eventTime: number) {
+    setState((state) => ({
+      ...state,
+      events: state.events.map((event) =>
+        event.time === eventTime ? { ...event, redacted: true } : event
+      ),
+    }));
+  }
+
+  function restoreEvent(eventTime: number) {
+    setState((state) => ({
+      ...state,
+      events: state.events.map((event) =>
+        event.time === eventTime ? { ...event, redacted: false } : event
+      ),
+    }));
+  }
+
+  function hideRedactedEvents() {
+    setState((state) => ({
+      ...state,
+      showRedactedEvents: false,
+    }));
+  }
+
+  function showRedactedEvents() {
+    setState((state) => ({
+      ...state,
+      showRedactedEvents: true,
+    }));
+  }
+
+  const toggleShowRedactedEventsButton = state.showRedactedEvents ? (
+    <button onClick={hideRedactedEvents}>Hide redacted</button>
+  ) : (
+    <button onClick={showRedactedEvents}>Show redacted</button>
+  );
+
+  return (
+    <>
+      <h2>
+        Events ({sortedEventsRecentFirst.length}){" "}
+        {toggleShowRedactedEventsButton}
+      </h2>
+      <ul>
+        {sortedEventsRecentFirst.map((event) => (
+          <li
+            key={event.time}
+            className={"Event" + (event.redacted ? " Event--redacted" : "")}
+          >
+            {event.name} {<EventTime time={event.time} />}{" "}
+            {event.redacted ? (
+              <button onClick={() => restoreEvent(event.time)}>Restore</button>
+            ) : (
+              <button onClick={() => redactEvent(event.time)}>Redact</button>
+            )}
+          </li>
+        ))}
       </ul>
     </>
   );
