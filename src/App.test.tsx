d28f11eaf4dbd1145de694dd8a0e56005662b374
Kyle Lin <kylejlin.us@gmail.com>
2023-02-10 23:32:18 -0800

    Updates

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-import { localStorageStateProvider } from "./stateProviders/localStorage";
-
-test("renders without crashing", () => {
-  render(<App stateProvider={localStorageStateProvider} />);
-});
--- src/components/EventsMenu/EventLine.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../../App.css";
-import { StateHookProps } from "../../types";
-
-export function EventLine({ stateHook }: StateHookProps) {
-  return <div className="PageMenu">TODO</div>;
-}
