11f18968b964cb43f9c10f3382dfc8f153cdbcf7
Kyle Lin <kylejlin.us@gmail.com>
2023-02-11 23:36:46 -0800

    Restyle stamp button

--- src/components/Stamp.tsx
+++ src/components/Stamp.tsx
@@ -33,7 +33,7 @@ export function Stamp({
           className="BarListItem__Button BarListItem__Button--stamp"
           onClick={addEventNow}
         >
-          ⬇️
+          +
         </button>
       )}
     </li>
