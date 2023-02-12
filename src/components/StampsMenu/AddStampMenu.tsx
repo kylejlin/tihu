d9a9d91c9e0bdaeed8214dc42a7a26410d9d01d8
Kyle Lin <kylejlin.us@gmail.com>
2023-02-12 00:24:53 -0800

    Prototype add stamp menu

--- src/components/StampsMenu/AddStampMenu.tsx
+++ src/components/StampsMenu/AddStampMenu.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 import "../../App.css";
 import { StateHookProps } from "../../types";
+import { Stamp } from "../Stamp";
 
 export function AddStampMenu({ stateHook }: StateHookProps) {
-  return <div className="PageMenu">TODO</div>;
+  const [state] = stateHook;
+  const todoIsValid = true;
+  return (
+    <div className="PageMenu--events__EventList">
+      <ul className="BarList BarList--containerFilling">
+        <li className="BarListItem BarListItem--stamp">
+          <input
+            className={
+              "BarListItem--event__TimeInput" +
+              (!todoIsValid ? " BarListItem--event__TimeInput--invalid" : "")
+            }
+            value={"todostampname"}
+            readOnly
+          />
+
+          <button className="BarListItem__Button BarListItem__Button--stamp">
+            {todoIsValid ? "✅" : "❌"}
+          </button>
+        </li>
+
+        {state.stamps.map((stamp) => (
+          <Stamp
+            key={stamp}
+            stateHook={stateHook}
+            stampName={stamp}
+            stampable={false}
+            deletable={false}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
