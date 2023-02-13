8ff15f3592ad6b55ccd64da7deee1a4176c90812
Kyle Lin <kylejlin.us@gmail.com>
2023-02-12 16:37:43 -0800

    Redesign StampsMenu

--- src/components/StampsMenu/index.tsx
+++ src/components/StampsMenu/index.tsx
@@ -1,26 +1,85 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { StampsMenuKind, StateHookProps } from "../../types";
 import "../../App.css";
-import { StampBarList } from "./StampBarList";
-import { AddStampMenu } from "./AddStampMenu";
-import { StampsMenuNavBar } from "./StampsMenuNavBar";
+import { StampBar } from "../StampBar";
+import { isTentativeNewStampValid } from "../../stateUtils";
 
 export function StampsMenu({ stateHook }: StateHookProps) {
-  const [state] = stateHook;
-  const activeMenu = ((): ReactNode => {
-    switch (state.stampsMenuKind) {
-      case StampsMenuKind.List:
-        return <StampBarList stateHook={stateHook} />;
-      case StampsMenuKind.Add:
-        return <AddStampMenu stateHook={stateHook} />;
-    }
-  })();
+  const [state, setState] = stateHook;
+  const sortedEventsRecentFirst = state.events
+    .slice()
+    .sort((a, b) => b.time - a.time);
+  const tentativeNewStampValid = isTentativeNewStampValid(state);
+
+  function editTentativeNewStamp(e: React.ChangeEvent<HTMLInputElement>) {
+    setState((state) => ({ ...state, tentativeNewStamp: e.target.value }));
+  }
+
+  function addTentativeNewStamp() {
+    setState((state) => ({
+      ...state,
+      stamps: state.stamps.concat([state.tentativeNewStamp]),
+      tentativeNewStamp: "",
+    }));
+  }
+
+  function clearTentativeStampAndNavigateToStampList() {
+    setState((state) => ({
+      ...state,
+      stampsMenuKind: StampsMenuKind.List,
+      tentativeNewStamp: "",
+    }));
+  }
 
   return (
-    <div className="PageMenu PageMenu--events">
-      {activeMenu}
+    <div className="PageMenu PageMenu--home">
+      <div className="PageMenu--home__Stamps">
+        <ul className="BarList BarList--containerFilling">
+          {state.stamps.map((stamp) => (
+            <StampBar
+              key={stamp}
+              stateHook={stateHook}
+              stamp={stamp}
+              eventKind={null}
+              deletable={true}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="PageMenu--home__PreviousEvent">
+        {sortedEventsRecentFirst.length > 0 && (
+          <li className="BarListItem BarListItem--stamp BarListItem--stamp--tentativeStamp">
+            <span className="BarListItem__Label">Add</span>
 
-      <StampsMenuNavBar stateHook={stateHook} />
+            <input
+              className={
+                "BarListItem--event__TimeInput" +
+                (!tentativeNewStampValid
+                  ? " BarListItem--event__TimeInput--invalid"
+                  : "")
+              }
+              value={state.tentativeNewStamp}
+              onChange={editTentativeNewStamp}
+            />
+
+            {tentativeNewStampValid ? (
+              <button
+                className="BarListItem__Button BarListItem__Button--stamp"
+                onClick={addTentativeNewStamp}
+              >
+                ✅
+              </button>
+            ) : (
+              <button
+                className="BarListItem__Button BarListItem__Button--stamp"
+                onClick={clearTentativeStampAndNavigateToStampList}
+              >
+                ❌
+              </button>
+            )}
+          </li>
+        )}
+      </div>
     </div>
   );
 }
