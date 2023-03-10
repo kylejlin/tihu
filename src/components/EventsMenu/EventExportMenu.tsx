10e6e1ba4ee3eabb01ec8e016f869856ffcb6fcb
Kyle Lin <kylejlin.us@gmail.com>
2023-03-09 20:53:55 -0800

    Support importing state

--- src/components/EventsMenu/EventExportMenu.tsx
+++ src/components/EventsMenu/EventExportMenu.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../../App.css";
-import { StateHookProps } from "../../types";
+import { State, StateHookProps } from "../../types";
 import utf8 from "utf8";
+import { CURRENT_STATE_VERSION } from "../../stateUtils";
 
 export function EventExportMenu({ stateHook }: StateHookProps) {
-  const [state] = stateHook;
+  const [state, setState] = stateHook;
 
   function exportState() {
     const s = JSON.stringify(state);
@@ -18,14 +19,47 @@ export function EventExportMenu({ stateHook }: StateHookProps) {
     a.click();
   }
 
+  function importEvents() {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".json";
+    input.addEventListener("change", (e) => {
+      const file = (e.target as HTMLInputElement).files?.[0];
+      if (file === undefined) {
+        return;
+      }
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        const { result } = reader;
+        if (typeof result !== "string") {
+          return;
+        }
+        try {
+          console.log(result);
+          const newState: State = JSON.parse(result);
+          if (newState.stateVersion !== CURRENT_STATE_VERSION) {
+            alert("Incompatible backup file.");
+            return;
+          }
+          const confirmation = window.prompt(
+            'Importing events will overwrite your current events. This is irreversible. Type "overwrite" to continue.'
+          );
+          if (confirmation === "overwrite") {
+            setState((state) => ({ ...state, events: newState.events }));
+          }
+        } catch {
+          alert("Invalid file.");
+        }
+      });
+      reader.readAsText(file);
+    });
+    input.click();
+  }
+
   return (
     <div className="FillContainerExceptLeaveRoomForBarListItemAtBottom">
       <button onClick={exportState}>Export</button>
-
-      <label>
-        <input type="file" onChange={() => alert("TODO")}></input>
-        Import
-      </label>
+      <button onClick={importEvents}>Import</button>
     </div>
   );
 }
